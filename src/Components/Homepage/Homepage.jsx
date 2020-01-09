a96b25050f333d9094b0da10f42429f1571a94dd
name <notdylanbozarth@gmail.com>
2020-01-09 09:56:31 -0800

    first commit

---
+++ src/Components/Homepage/Homepage.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import './homepage.css'
+
+export const Homepage = () => (
+  <div className="homepage">
+    Replica Shop 
+    <div className="directory-menu">
+      I am the menu
+      <div className="menu-item">
+
+        <div className="content">
+           <h1 className="title">Neolithic era <br /> </h1> <p>10,000–4,500 BC</p>
+           <span className="subtitle">Shop for books</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+
+        <div className="content">
+           <h1 className="title">Bronze age <br /> </h1> <p>Some time </p>
+           <span className="subtitle">Shop for replicas and books</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+
+        <div className="content">
+           <h1 className="title">Iron age <br /> </h1> <p>Some time </p>
+           <span className="subtitle">Shop for replicas and books</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+    
+        <div className="content">
+           <h1 className="title">Early Middle ages <br /> </h1> <p>Some time </p>
+           <span className="subtitle">Shop for replicas and books</span>
+        </div>
+      </div>
+
+
+      <div className="menu-item">
+    
+        <div className="content">
+           <h1 className="title">Late Middle ages <br /> </h1> <p>Some time </p>
+           <span className="subtitle">Shop for replicas and books</span>
+        </div>
+      </div>
+
+      <div className="menu-item">
+        <div className="content">
+           <h1 className="title">Age of exploration <br /> </h1> <p>Some time </p>
+           <span className="subtitle">Shop for replicas and books</span>
+        </div>
+      </div>
+<h1 className="divider"><br /> Other </h1>
+      <div className="menu-item2">
+        <div className="content">
+           <h1 className="title">Historical Archery <br /> </h1> <p>Some time </p>
+           <span className="subtitle">Shop for replicas and equipment</span>
+        </div>
+      </div>
+
+      <div className="menu-item2">
+        <div className="content">
+           <h1 className="title">Fantasy weapons and armor <br /> </h1> <p>From various series </p>
+           <span className="subtitle">Shop for replicas</span>
+        </div>
+      </div>
+
+    </div>
+  </div>
+);
